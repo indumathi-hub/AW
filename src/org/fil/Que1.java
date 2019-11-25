cdc16b1ffcf94c157c3944622c552cbf38577242
indumathi-hub <indurag77@gmail.com>
2019-11-26 00:02:10 +0530

    new code

--- src/org/fil/Que1.java
+++ src/org/fil/Que1.java
@@ -10,11 +10,6 @@ public class Que1 {
 		File f=new File("D:\\indu");
 		boolean b = f.mkdir();
 		System.out.println(b);
-		File f1=new File("D:\\indu\\rag.txt");
-		boolean c = f1.createNewFile();
-		System.out.println(c);
-		File f2=new File("D:\\indu\\rag.txt");
-		FileUtils.write(f2, "Greens Technology is recognized for its Selenium Training in both retail and corporate market. We have trained above 28,000 students exclusively on QTP and Selenium in last 7 years.All our trainers’ expertises on both development and training which helps us deliver project based training.");
 	}
 
 }
